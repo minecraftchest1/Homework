9980913be9ce14ed307087f53e47434efa26488f
Minecraftchest1 <minecraftchest1@outlook.com>
2021-04-28 10:12:13 -0500

    Create build scripts.

---
+++ PythagoreanTheormCalc/Build.ps1
@@ -0,0 +1,6 @@
+﻿dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r win-arm64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r linux-arm64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r linux-arm --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
---
+++ PythagoreanTheormCalc/Build.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+dotnet publish -c Release -r win-x64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r win-arm64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r linux-arm64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r linux-arm --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
+dotnet publish -c Release -r osx-x64 --self-contained true /p:PublishTrimmed=true -p:PublishSingleFile=true
